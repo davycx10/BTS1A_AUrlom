74b5939bc26491f6f222dc004786306b18711ee7
davycx <dave.israel785@gmail.com>
2025-06-20 15:09:17 +0200

    add id for article test selenium

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $searchcrit = "";
 
             <div class="droite">
                 <div style="font-size : 32px"><?= $artprix ?> €</div>
-                <div><a href="panier.php?id=<?=$artid ?>"><img src='Logo/basket.png' height=36 /></a></div>
+                <div id=<?=$artid ?>><a href="panier.php?id=<?=$artid ?>"><img src='Logo/basket.png' height=36 /></a></div>
             </div>
         </div>
 
